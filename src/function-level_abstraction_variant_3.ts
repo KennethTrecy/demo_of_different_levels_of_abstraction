a170ee1d8c9cc3559d2a5a1dbf37450a1120df5f
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2023-02-07 09:48:24 +0800

    feat: make third example of function-level abstraction

---
+++ src/function-level_abstraction_variant_3.ts
@@ -0,0 +1,54 @@
+import { stdin, stdout } from "process"
+import { config } from "dotenv"
+
+config()
+
+const DEFAULT_FIRST_NAME_QUESTION = "What is your first name? "
+const DEFAULT_LAST_NAME_QUESTION = "What is your last name? "
+const DEFAULT_AGE_QUESTION = "What is your age? "
+const DEFAULT_MONEY_QUESTION = "How much do you have? "
+const ENCODING = "utf-8"
+
+async function readInput(question: string): Promise<string> {
+	let input = ""
+
+	await new Promise<void>(resolve => {
+		stdout.write(question)
+		stdin.on("readable", function readInput() {
+			input = stdin.read().toString(ENCODING)
+			stdin.off("readable", readInput)
+			resolve()
+		})
+	})
+
+	return input
+}
+
+async function readStringInput(question: string): Promise<string> {
+	const input = await readInput(question)
+	return input.trim()
+}
+
+async function readNumericInput(question: string): Promise<number> {
+	const input = await readInput(question)
+
+	return Number(input)
+}
+
+async function main() {
+	const firstNameQuestion = process.env.FIRST_NAME_QUESTION || DEFAULT_FIRST_NAME_QUESTION
+	const firstName = await readStringInput(firstNameQuestion)
+
+	const lastNameQuestion = process.env.LAST_NAME_QUESTION || DEFAULT_LAST_NAME_QUESTION
+	const lastName = await readStringInput(lastNameQuestion)
+
+	const ageQuestion = process.env.AGE_QUESTION || DEFAULT_AGE_QUESTION
+	const age = await readNumericInput(ageQuestion)
+
+	const moneyQuestion = process.env.MONEY_QUESTION || DEFAULT_MONEY_QUESTION
+	const money = await readNumericInput(moneyQuestion)
+
+	stdout.write(`You are ${firstName} ${lastName}. Your age is ${age} and you have $${money}.\n`)
+}
+
+main()
